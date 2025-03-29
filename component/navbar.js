5fb24bef72000be35b675b3c92e0f1c2ea28425c
mosetz <saritsamkumpim@gmail.com>
2025-03-29 17:08:03 +0000

    update nav bar and create folder for storing img

--- component/navbar.js
+++ component/navbar.js
@@ -6,27 +6,66 @@ class NavBar extends HTMLElement{
 
     connectedCallback(){
         this.shadowRoot.innerHTML = `
-        <style>${this.getStyle}</style>
+        <style>${this.getStyle()}</style>
         <nav id="main-nav" class="nav-bar">
-            <figure class="nav-logo"></figure>
+            <figure class="nav-logo"><img src="./image/popmartlogo.png"></figure>
             <ul class="nav-list">
-                <a>New & Feature</a>
-                <a>SERIES</a>
-                <a>TYPES</a>
-                <a>EVENTS</a>
-                <a>STORE</a>
-                <a>ABOUT US</a>
+                <li><a href="#">New & Feature</a></li>
+                <li><a href="#">SERIES</a></li>
+                <li><a href="#">TYPES</a></li>
+                <li><a href="#">EVENTS</a></li>
+                <li><a href="#">STORE</a></li>
+                <li><a href="#">ABOUT US</a></li>
             </ul>
+            <div class="nav-log">
+                <img src="./image/person.png" alt="User Icon"/> <a href="#" class="link-log">Sign in / Register</a>
+            </div>
         </nav>
-        <div class="nav-search">
-            <input type="text" placeholder="Search...">
-            <button>Search</button>
-        </div>
         `
     }
     
     getStyle(){
-        return``
+        return`
+        nav {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 10px 20px;
+            background: white;
+            box-shadow: 0 1px 6px rgba(0,0,0,0.1);
+            font-family: sans-serif;
+        }
+        
+        .nav-logo img {
+            hight: 80px;
+            width: 80px;
+        }
+
+        .nav-list {
+            display: flex;
+            list-style: none;
+            gap: 20px;
+        }
+
+        .nav-list li a {
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+            font-size: 16px;
+        }
+        
+        .nav-log a {
+            text-decoration: none;
+            color: black;
+            font-size: 16px;
+        }
+
+        .nav-log img {
+            width: 15px;
+            height: 15px;
+            background-color: black;
+        }
+        `
     }
 
 
