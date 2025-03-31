fad7335b30bbe3878b1f204c47ff17a746766f5a
mosetz <saritsamkumpim@gmail.com>
2025-03-31 22:38:30 +0100

    update search box style and icon

--- component/searchbar.js
+++ component/searchbar.js
@@ -6,6 +6,7 @@ class SearchBar extends HTMLElement {
 
     connectedCallback(){
         this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
         <style>${this.getStyle()}</style>
         <div class="search-cont">
             <input type="text" class="search-bar" placeholder="Look for your favorite toys..." >
@@ -16,6 +17,34 @@ class SearchBar extends HTMLElement {
 
     getStyle(){
         return`
+        .search-cont {
+            display: flex;
+            flex-direction: row;
+            position: relative;
+            display: inline-block;
+        }
+
+        .search-cont input {
+            padding: 3px 10px;
+            border-radius: 20px; 
+            flex-grow: 3;
+            border:  grey;
+            background: lightgrey;
+
+        }
+
+        .search-cont button {
+            position: absolute;
+            top: 0;
+            right: 0;
+            border: none;
+            background: none;
+            color: grey;
+            cursor: pointer;
+            items-align: center;
+        }
+
+        
         `
     }
     
